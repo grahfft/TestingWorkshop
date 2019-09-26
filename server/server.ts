1091cddca5acee38652dd08f7a07a66c4a8d3623
Dennis Lassell <dennis.lassel@gmail.com>
2019-09-26 14:52:51 -0400

    added script to get server and frontend running at same time

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+
 const app = express()
 const port = 4000
+  
+app.get('/', (req, res) => res.send(`🌈 This thang LIT on port ${port} 🌈`))
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, async () => {
+    console.log(`🌈 This thang LIT on port ${port} 🌈`);
+});
